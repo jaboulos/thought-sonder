0cfce990a00561868cbabc04db1102ced54f54e6
jaboulos <jab12288@gmail.com>
2021-03-18 15:45:16 -0700

    Add config for mikroORM cli

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+
+// Can access this info from the mikroORM cli
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'), // path to the folder with migrations.  Path creates absolute path and joins these two file paths
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files.  [tj]s allows ts and js files.
+  },
+  entities: [Post], // Corresponds to all db tables
+  dbName: 'lireddit',
+  user: 'postgres',
+  password: 'password',
+  type: 'postgresql',
+  debug: !__prod__, // Logs what sql is being executed underneath the hood.  Helpful for debugging.
+  // MikroORM is a function and I'm giving it the type of function...Parameters returns an array, so give it first item in array
+  // doing this so the 'const orm = await MikroORM.init(mikroConfig);' line in index.ts stops complaining
+} as Parameters<typeof MikroORM.init>[0]; // Put a type in the brackets and it will tell you all the parameters for it
