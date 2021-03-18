bfbd3a34a191d25982e21b8a8e71fe077f2a0c29
jaboulos <jab12288@gmail.com>
2021-03-18 16:27:00 -0700

    Add reference notes for running some sql with mikroORM

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,20 @@ import mikroConfig from './mikro-orm.config';
 
 // Create this function 'main' to resolve promises for the methods within.
 const main = async () => {
+  // Connect to db
   const orm = await MikroORM.init(mikroConfig);
+  // Automatically run migrations
+  await orm.getMigrator().up();
 
-  // Create a post
-  const post = orm.em.create(Post, { title: 'my first post' });
-  // Insert post into db
-  await orm.em.persistAndFlush(post);
+  // Run some sql - ex: Create a post
+  // const post = orm.em.create(Post, { title: 'my first post' });
+
+  // Insert post into table
+  // await orm.em.persistAndFlush(post);
+
+  // Get all posts and console log them
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch((err) => {
