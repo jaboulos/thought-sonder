731e212b4c44e45de931738ff006d1e3ddd37a2e
jaboulos <jab12288@gmail.com>
2021-03-29 10:57:10 -0700

    Pass all props to InputField component

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -15,14 +15,23 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _, // strip size off of props, not passing size in, renamed to _, dont need to rename though
+  ...props
+}) => {
   // formik hook
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
       {/* dynamic input, can handle any field */}
-      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
