4f980d63d0f81d4f7f05ebe353974423dfc76b6c
jaboulos <jab12288@gmail.com>
2021-03-29 10:58:24 -0700

    Wrap app with urql provider, point client to gql server, set credentials config for cookie

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,19 +1,31 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { Provider, createClient } from 'urql';
 
-import theme from '../theme'
+// point to GQL server
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+  fetchOptions: {
+    // This will send a cookie, need this for getting a cookie when we register or login
+    credentials: 'include',
+  },
+});
+
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
-  )
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
