aea4f5e08d5d7a33edc21db9d0563d6c7166ecfc
jaboulos <jab12288@gmail.com>
2021-03-30 14:49:31 -0700

    Add custom graphql hook userRegisterMutation

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,44 +1,34 @@
 import React from 'react';
 import { Form, Formik } from 'formik';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/core';
 
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
-import { useMutation } from 'urql';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
 interface registerProps {}
-
-// From GQL playground
-const REGISTER_MUT = `
-mutation Register($username:String!, $password:String!){
-  register(options: { username: $username, password: $password }) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}
-`;
 
 export const Register: React.FC<registerProps> = ({}) => {
   // urql hook
   // first item in array is some info about whats going on about the mutation, if not passing anything in, just leave as empty obj or put a comma
   // second item in array is our function, can name it anything
-  const [, register] = useMutation(REGISTER_MUT);
+  const [, register] = useRegisterMutation();
   return (
     <Wrapper variant='small'>
       {/* initial state of form */}
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          console.log(values);
-          // the values line up (username === username, so can just leave in as values, otherwise may look like this ex: ({username: values.user})) 2:38
-          // This is a promise, need to add a return so that it is returned once the promise resolves
-          return register(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            // if theres an error
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // works if get a user back after registering
+            // navigate them to the landing page
+            console.log(response.data.register.user);
+          }
         }}
       >
         {({ isSubmitting }) => (
@@ -58,7 +48,7 @@ export const Register: React.FC<registerProps> = ({}) => {
             </Box>
             <Button
               mt={4}
-              colorScheme='teal'
+              variantColor='teal'
               isLoading={isSubmitting}
               type='submit'
             >
